32b092d69cc640449b61218d9f8465038d1ff76e
willmichael <oleemir@gmail.com>
2017-12-05 01:51:04 -0800

    fixing account

--- account.php
+++ account.php
@@ -91,11 +91,6 @@ include('session.php');
                 ?>
             <p><a class="btn btn-secondary" href="team.php" role="button">View More &raquo;</a></p>
           </div>
-          <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div>
         </div>
 
         <hr>
